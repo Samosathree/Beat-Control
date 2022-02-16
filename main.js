9fb5a442ba26c3e21d47bad1074b9c26b0f5005f
Samosathree <84139052+Samosathree@users.noreply.github.com>
2022-02-15 19:51:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 Kda = "";
 Itzy = "";
+
 function preload(){
     Kda = loadSound("DrumGoDum.mp3");
     Itzy = loadSound("Icy.mp3");
 }
+
 function setup(){
     canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
-} function draw(){
+} 
+
+function draw(){
     image(video, 0, 0, 600, 500);
 }
